64e128361a1162e49da5fb78ab63be41df9df8e8
DevCharles <charlesipn@gmail.com>
2022-11-11 18:50:46 -0600

    PORT from env

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import dbConnect from './src/libs/db.js'
-import { server } from './src/server.js'
+import dbConnect from "./src/libs/db.js";
+import { server } from "./src/server.js";
 
 dbConnect()
   .then(() => {
-    console.log('Database connected c:')
+    console.log("Database connected c:");
 
-    server.listen(8080, () => {
-      console.log('Server listening on port 8080')
-    })
+    server.listen(process.env.PORT || 8080, () => {
+      console.log("Server listening on port 8080");
+    });
   })
-  .catch((error) => console.log('Error: ', error))
+  .catch((error) => console.log("Error: ", error));
